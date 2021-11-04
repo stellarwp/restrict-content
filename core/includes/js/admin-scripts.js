493b8a33dc0ec3173edec5fe7088e3aacaa23706
Jared Hill <jthill@JTHILL-LAPTOP.local>
2021-11-04 15:17:43 -0400

    adding the switch jquery click to admin js

--- core/includes/js/admin-scripts.js
+++ core/includes/js/admin-scripts.js
@@ -826,6 +826,26 @@ jQuery(document).ready(function($) {
 	$( "#rcp-code" ).on( 'focusout', function() {
 		$(this).val($(this).val().replace(/[^a-z0-9]/gi, ''));
 	});
+
+	$( '#restrict_content_legacy_switch' ).on( 'click', function() {
+		$.ajax({
+			data: {
+				action: 'rc_process_legacy_switch',
+				rc_process_legacy_nonce: $( '#rcp_settings_nonce').val(),
+			},
+			type: "post",
+			url: rcp_admin_settings_options.ajax_url,
+			success: function( response ) {
+				console.dir(response);
+				if ( response.success ) {
+					window.location.assign( '/wp-admin/admin.php?page=rcp-settings' );
+				}
+			},
+			error: function( response ) {
+				console.dir( response );
+			}
+		});
+	});
 });
 
 let RCP_Settings = {
