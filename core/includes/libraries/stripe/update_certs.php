52a2d5ea8db9af79c6fb6992b15aa0bc3fbb93a2
Israel Barragan <reedyseth@gmail.com>
2023-01-16 18:22:01 -0600

    Update Stripe library files to version 10.3

--- core/includes/libraries/stripe/update_certs.php
+++ core/includes/libraries/stripe/update_certs.php
@@ -4,12 +4,12 @@
 
 \set_time_limit(0); // unlimited max execution time
 
-$fp = \fopen( __DIR__ . '/data/ca-certificates.crt', 'w+b');
+$fp = \fopen(__DIR__ . '/data/ca-certificates.crt', 'w+b');
 
 $options = [
     \CURLOPT_FILE => $fp,
     \CURLOPT_TIMEOUT => 3600,
-    \CURLOPT_URL => 'https://curl.haxx.se/ca/cacert.pem',
+    \CURLOPT_URL => 'https://curl.se/ca/cacert.pem',
 ];
 
 $ch = \curl_init();
