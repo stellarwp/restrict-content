de999caf856fd84cd375c56ab2f16bce8f39c773
Israel Barragan <reedyseth@gmail.com>
2023-06-07 15:25:46 -0600

    Sync Core code with RCP main branch - 4c31cfc7.

--- core/includes/class-restrict-content.php
+++ core/includes/class-restrict-content.php
@@ -26,7 +26,7 @@ if ( ! class_exists( 'Restrict_Content_Pro' ) ) :
 	 * @since 3.0
 	 */
 	final class Restrict_Content_Pro {
-		const VERSION = '3.5.31';
+		const VERSION = '3.5.32';
 
 		/**
 		 * Stores the base slug for the extension.
